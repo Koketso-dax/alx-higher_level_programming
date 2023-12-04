4963ccaf3ece5b353c4feed9c457e45c178d54c6
Koketso-dax <lanchaster.k@gmail.com>
2023-12-04 22:25:51 +0200

    Added data structures

---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+# Using script to imoprt python modules
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Custom function to add 2 vars
+
+    Args:
+        a: input
+        b: input
+    Return:
+        a + b
+    """
+    return (a + b)
