756862107338983bf9be65fae241c576418b38b5
Koketso-dax <lanchaster.k@gmail.com>
2023-12-06 12:26:20 +0200

    Fixed output for 3-infinity add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,12 +5,11 @@ if __name__ != "__main__":
     exit()
 
 argc = len(sys.argv) - 1
-
 i = 0
 result = 0
-for arg in sysargv:
+for arg in sys.argv:
     if i != 0:
         result += int(arg)
     else:
         i += 1
-    print("{:d}".format(result))
+print("{:d}".format(result))
