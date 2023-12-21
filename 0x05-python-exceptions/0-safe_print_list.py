2ff48400a8a04f474d474b80eb01a945b732b8ee
Koketso-dax <lanchaster.k@gmail.com>
2023-12-21 04:53:21 +0200

    Fixed errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,13 +2,12 @@
 
 
 safe_print_list(my_list=[], x=0):
-    y = 0
-    displayed = 0
-    for y in range(0, x):
-        try:
-            print("{}".format(my_list[y]), end="")
-            displayed += 1
-        except:
-            continue
+    i = 0
+    try:
+        while i < x:
+            print("{:d}".format(my_list[i]), end="")
+            i += 1
+    except:
+        pass
     print()
-    return displayed
+    return i
