6ffb9b881bdd7887400e0dbb02bcab161bf7cc3e
Koketso-dax <lanchaster.k@gmail.com>
2024-03-11 20:43:33 +0200

    Fixed 7 bug

--- 0x12-javascript-warm_up/5-to_interger.js
+++ 0x12-javascript-warm_up/5-to_interger.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-console.log(parseInt(process.argv[2]) ? `My number: ${parseInt(process.argv[2])}` : 'Not a number');
+const arg = process.argv[2];
+const num = parseInt(arg);
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log('Not a number');
+}
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const vars = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+vars.forEach((element) => {
+  console.log(element);
+});
