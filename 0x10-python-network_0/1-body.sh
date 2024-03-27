d26a7ba92430f73ffe0e54d67ab3095e318da691
Koketso-dax <lanchaster.k@gmail.com>
2024-03-27 16:49:36 +0200

    A bunch of curl excercises

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays the size of body in response field
-curl -s -w "%{http_code}" -o /dev/stderr <URL> | { [ $(tail -n1 /dev/stderr) -eq 200 ] && cat /dev/stdin; }
+curl -s -w "%{http_code}" -o /dev/stderr "$1" | { [ "$(tail -n1 /dev/stderr)" -eq 200 ] && cat; }
