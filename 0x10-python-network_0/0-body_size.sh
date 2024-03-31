803648027f0195fd4f9eedfa48f03e32f98d911a
Koketso-dax <lanchaster.k@gmail.com>
2024-03-31 12:34:19 +0200

    curl exes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Fetches the body of the response for a given URL if the response status code is 200
-curl -s -o /tmp/response_body -w "%{http_code}" "$1" | { [ "$(tail -n1 /tmp/response_body)" -eq 200 ] && cat /tmp/response_body; }
+# send a request to an URL with curl, and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
