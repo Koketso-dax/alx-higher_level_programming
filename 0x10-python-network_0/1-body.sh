8fd368749c39918e29218cfb08a2e8e99eb69cc6
Koketso-dax <lanchaster.k@gmail.com>
2024-03-31 12:22:38 +0200

    curl to the end with redirects 2

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Fetches the body of the response for a given URL if the response status code is 200
-curl -s -L -o /tmp/response_body -w "%{http_code}" "$1" | { [ "$(tail -n1 /tmp/response_body)" -eq 200 ] && cat /tmp/response_body; }
+curl -s -L -o /tmp/response_body -w "%{http_code}" --max-redirs 5 "$1" | { [ "$(tail -n1 /tmp/response_body)" -eq 200 ] && cat /tmp/response_body; }
