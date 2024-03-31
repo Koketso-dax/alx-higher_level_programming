185ba18048b820f29dfb2e14ed2be4d9a8afc0e6
Koketso-dax <lanchaster.k@gmail.com>
2024-03-31 12:38:31 +0200

    curl exes

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept
-curl -sI --head "$1" | grep -i Allow
+curl -sI --head "$1" | grep -i Allow | sed 's/Allow: //i' | tr -d '\r' | xargs -n1
