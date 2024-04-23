a2964b60d90fc772e5d0d12e1a8f708e7f3dd1a8
Koketso-dax <lanchaster.k@gmail.com>
2024-04-23 15:42:21 +0200

    starwars characters

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,26 +1,36 @@
 #!/usr/bin/node
-
 const request = require('request');
 
+// Check if movie ID is provided
+if (process.argv.length < 3) {
+    console.error('Please provide the movie ID as an argument.');
+    process.exit(1);
+}
+
 const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  const characters = data.characters;
-  for (const character of characters) {
-    request(character, (error, response, body) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
-  }
+// Make a request to the Star Wars API to get movie details
+request(apiUrl, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+    } else if (response.statusCode !== 200) {
+        console.error('Unexpected status code:', response.statusCode);
+    } else {
+        const movie = JSON.parse(body);
+        console.log(`Characters of ${movie.title}:`);
+        movie.characters.forEach(characterUrl => {
+            // Fetch each character's details
+            request(characterUrl, (charError, charResponse, charBody) => {
+                if (charError) {
+                    console.error('Error:', charError);
+                } else if (charResponse.statusCode !== 200) {
+                    console.error('Unexpected status code:', charResponse.statusCode);
+                } else {
+                    const character = JSON.parse(charBody);
+                    console.log(character.name);
+                }
+            });
+        });
+    }
 });
